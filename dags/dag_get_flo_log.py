8892618c2d31fb863b652839aa6bb1bc72951b86
wlsklee98 <wlsklee98@gmail.com>
2024-01-31 10:36:17 +0900

    new dag for flo check

--- dags/dag_get_flo_log.py
+++ dags/dag_get_flo_log.py
@@ -30,13 +30,51 @@ default_args = {
 yesterday = (datetime.today() - timedelta(days=1)).strftime('%Y%m%d')
 
 with DAG(
-    "flo_log",
+
+    "flo_log", #name of dag appear in ui
     default_args=default_args,
     description="DAG with own plugins",
-    schedule="0 9 * * *",
+    schedule="45 10 * * *",
     start_date=pendulum.datetime(2023, 1, 1, tz=local_tz),
     catchup=True,
     max_active_runs=5,
     tags=["test"],
     
-) as dag:   
+) as dag:
+    dag.doc_md = __doc__
+
+    yyyy = '{{ ds_nodash[:4] }}'
+    mm = '{{ ds_nodash[4:6] }}'
+    dd = '{{ ds_nodash[6:8] }}'
+    
+    meta_path = f"/data/music/transform/flo/track_meta/{yesterday}/_SUCCESS"
+    
+    start = DummyOperator(task_id='start', dag=dag)
+    intermediate = DummyOperator(task_id='intermediate', dag=dag)
+    end = DummyOperator(task_id='end', dag=dag)
+    
+    
+    
+    flo_meta_sensor = WebHdfsSensor(
+        task_id="flo_meta_sensor",
+        webhdfs_conn_id='tidcex_hadoop',
+        filepath=meta_path,
+        poke_interval=60 * 1,
+        timeout=60 * 60 * 24,
+        dag=dag )
+    
+    
+    test1 = NesOperator(
+       task_id="test_task1",
+       parameters={"current_dt": "{{ ds_nodash }}"},
+       input_nb="./notebook/sense_test.ipynb",
+)
+    
+    
+    
+    
+    start >> flo_meta_sensor
+    
+    flo_meta_sensor >> test1
+    
+    test1 >> end
